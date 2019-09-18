ea380d1f89b2c9d5b3af67fa94954d90315734de
lllverb <wktkm9orz2@gmail.com>
2019-09-18 16:59:29 +0900

    commit

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(*Rails.groups)
 
 module QuizApp
   class Application < Rails::Application
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
